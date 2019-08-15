7bafc9cba086b7bde149d8a30f3ab5e2e6b6ecd5
Sairam Ghanta <sairam.ghanta.2@consultant.volvo.com>
2019-08-15 14:14:10 +0200

    added tests and fixed api proj

---
+++ Gold_API/API.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace Gold_API
+{
+    public class Api
+    {
+        static string data = Gold_API.Resources.data;
+        public double GetPrice(int index)
+        {
+            var records= GetRecords();
+
+            int actualRow = index;
+            if (actualRow == records.Count)
+            {
+                actualRow = index-1;
+            }
+            if (actualRow < 0)
+            {
+                throw new InvalidDataException("Index Can't be negative");
+            }
+            if (actualRow >= records.Count)
+            {
+                throw new InvalidDataException("Index Can't be more than "+(records.Count-1));
+            }
+
+            return records[actualRow].Price;
+        }
+
+        private static List<Gold> GetRecords()
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(data);
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            using (var reader = new StreamReader(stream))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.HasHeaderRecord = false;
+                return new List<Gold>(csv.GetRecords<Gold>());
+            }
+        }
+        public class Gold
+        {
+            public DateTime Date { get; set; }
+            public double Price { get; set; }
+        }
+    }
+}
---
+++ Tests/API_Tests/APITest.cs
@@ -0,0 +1,32 @@
+﻿using Gold_API;
+using NUnit.Framework;
+
+namespace API_Tests
+{
+    [TestFixture]
+    public class ApiTest
+    {
+        private Api _api;
+
+        [SetUp]
+        public void Setup()
+        {
+            _api = new Api();
+        }
+
+        [Test]
+        public void FirstRecordTest()
+        {
+            double expected = 34.73;
+            double actual = _api.GetPrice(0);
+            Assert.AreEqual(expected,actual);
+        }
+        [Test]
+        public void LastRecordTest()
+        {
+            double expected = 1414.611;
+            double actual = _api.GetPrice(835);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
